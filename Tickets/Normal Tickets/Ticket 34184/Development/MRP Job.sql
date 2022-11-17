361e693eb9490bd0370dc35a8de8fc8b7f6cacec
Justin Pope <JustinP@summerclassics.com>
2022-11-17 15:24:14 -0600

    dev 20221117 1524:

--- Tickets/Normal Tickets/Ticket 34184/Development/MRP Job.sql
+++ Tickets/Normal Tickets/Ticket 34184/Development/MRP Job.sql
@@ -1,11 +1,15 @@
 USE [msdb]
 GO
 
-/****** Object:  Job [Load MRP]    Script Date: 11/17/2022 2:38:29 PM ******/
+/****** Object:  Job [Update InvControl GtrStructureChange]    Script Date: 11/17/2022 3:16:52 PM ******/
+EXEC msdb.dbo.sp_delete_job @job_name =N'Update InvControl GtrStructureChange', @delete_unused_schedule=1
+GO
+
+/****** Object:  Job [Update InvControl GtrStructureChange]    Script Date: 11/17/2022 3:16:52 PM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 11/17/2022 2:38:29 PM ******/
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 11/17/2022 3:16:52 PM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
@@ -21,14 +25,29 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Load MRP',
 		@notify_level_netsend=0, 
 		@notify_level_page=0, 
 		@delete_level=0, 
-		@description=N'Job runs the following procedures in the following order: 1) usp_LoadHistory 2) usp_LoadProfiles 3) usp_LoadProfileStatistics 4) usp_LoadPareto', 
+		@description=N'No description available.', 
 		@category_name=N'[Uncategorized (Local)]', 
 		@owner_login_name=N'SUMMERCLASSICS\SqlAgentUser', 
 		@notify_email_operator_name=N'Database Administrators', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Load History]    Script Date: 11/17/2022 2:38:29 PM ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load History', 
+/****** Object:  Step [PRODUCT_INFO.dbo.usp_Update_InvControl_GtrStructureChange_Company_100]    Script Date: 11/17/2022 3:16:52 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'PRODUCT_INFO.dbo.usp_Update_InvControl_GtrStructureChange_Company_100', 
 		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'EXECUTE PRODUCT_INFO.dbo.usp_Update_InvControl_GtrStructureChange_Company_100;', 
+		@database_name=N'PRODUCT_INFO', 
+		@flags=12
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Load History]    Script Date: 11/17/2022 3:18:25 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load History', 
+		@step_id=2, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -41,9 +60,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load His
 		@database_name=N'SysproCompany100', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Load Profiles]    Script Date: 11/17/2022 2:38:29 PM ******/
+/****** Object:  Step [Load Profiles]    Script Date: 11/17/2022 3:18:25 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load Profiles', 
-		@step_id=2, 
+		@step_id=3, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -56,9 +75,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load Pro
 		@database_name=N'SysproCompany100', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Load Profile Statistics]    Script Date: 11/17/2022 2:38:29 PM ******/
+/****** Object:  Step [Load Profile Statistics]    Script Date: 11/17/2022 3:18:25 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load Profile Statistics', 
-		@step_id=3, 
+		@step_id=4, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -71,9 +90,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load Pro
 		@database_name=N'SysproCompany100', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Load Pareto]    Script Date: 11/17/2022 2:38:29 PM ******/
+/****** Object:  Step [Load Pareto]    Script Date: 11/17/2022 3:18:25 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load Pareto', 
-		@step_id=4, 
+		@step_id=5, 
 		@cmdexec_success_code=0, 
 		@on_success_action=1, 
 		@on_success_step_id=0, 
@@ -88,19 +107,19 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Load Par
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Monday''s at 4am', 
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Monday 4:00am', 
 		@enabled=1, 
 		@freq_type=8, 
-		@freq_interval=3, 
+		@freq_interval=1, 
 		@freq_subday_type=1, 
 		@freq_subday_interval=0, 
 		@freq_relative_interval=0, 
 		@freq_recurrence_factor=1, 
-		@active_start_date=20221117, 
+		@active_start_date=20170425, 
 		@active_end_date=99991231, 
 		@active_start_time=40000, 
 		@active_end_time=235959, 
-		@schedule_uid=N'eed9ab17-f02d-46be-9048-a20c4d9dbc79'
+		@schedule_uid=N'ef8b7c0c-23aa-43a8-a01b-961534de6409'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
