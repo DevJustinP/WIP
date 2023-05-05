7202a6c7743d7cedf580c95225481a29d19844dc
Justin Pope <JustinP@summerclassics.com>
2023-05-05 11:33:06 -0500

    changes

---
+++ Tickets/Normal Tickets/Ticket 34810 - Customer Services Rep/Production/create Update Customer Service Rep Reference.sql
@@ -0,0 +1,102 @@
+USE [msdb]
+GO
+
+/****** Object:  Job [Update Customer Service Rep Reference]    Script Date: 5/5/2023 11:30:35 AM ******/
+EXEC msdb.dbo.sp_delete_job @job_name=N'Update Customer Service Rep Reference', @delete_unused_schedule=1
+GO
+
+/****** Object:  Job [Update Customer Service Rep Reference]    Script Date: 5/5/2023 11:30:35 AM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 5/5/2023 11:30:35 AM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Update Customer Service Rep Reference', 
+		@enabled=1, 
+		@notify_level_eventlog=2, 
+		@notify_level_email=2, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'SUMMERCLASSICS\SqlAgentUser', 
+		@notify_email_operator_name=N'Database Administrators', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [PRODUCT_INFO.dbo.usp_Update_Reference_CustomerServiceRep]    Script Date: 5/5/2023 11:30:36 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'PRODUCT_INFO.dbo.usp_Update_Reference_CustomerServiceRep', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'EXEC PRODUCT_INFO.dbo.usp_Update_Reference_CustomerServiceRep', 
+		@database_name=N'master', 
+		@flags=12
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [PRODUCT_INFO.dbo.usp_Update_Reference_CustomerServiceRep OLD]    Script Date: 5/5/2023 11:30:36 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'PRODUCT_INFO.dbo.usp_Update_Reference_CustomerServiceRep OLD', 
+		@step_id=2, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=3, 
+		@retry_interval=1, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'EXECUTE PRODUCT_INFO.dbo.usp_Update_Reference_CustomerServiceRep;', 
+		@database_name=N'PRODUCT_INFO', 
+		@flags=12
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 2
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Weekdays at 12:30 PM', 
+		@enabled=1, 
+		@freq_type=8, 
+		@freq_interval=62, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=1, 
+		@active_start_date=20180124, 
+		@active_end_date=99991231, 
+		@active_start_time=123000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'4f281837-a1bb-4a89-a13e-f910d4ef64da'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Weekdays at 7:30 AM', 
+		@enabled=1, 
+		@freq_type=8, 
+		@freq_interval=62, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=1, 
+		@active_start_date=20180124, 
+		@active_end_date=99991231, 
+		@active_start_time=73000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'1050bd7e-61fe-415c-920e-1f2fdf09d1f7'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
+
