b8dcdf443241e8c90b4b205ded9f4539b86888fa
Justin Pope <pope2014@gmail.com>
2023-05-26 11:36:44 -0500

    changes for SDM 37223

--- Tickets/Normal Tickets/Ticket 37223 - Job for Deletions/Deployment/Deployment.sql
+++ Tickets/Normal Tickets/Ticket 37223 - Job for Deletions/Deployment/Deployment.sql
@@ -1,7 +1,7 @@
 Use [PRODUCT_INFO]
 go
 
-if exists(select 1 from [PRODUCT_INFO].[sys].[tables] where [tables].[name] ='SalesOrderLineDelete_Ref')
+if not exists(select 1 from [PRODUCT_INFO].[sys].[tables] where [tables].[name] ='SalesOrderLineDelete_Ref')
 begin
 	create table [SugarCrm].[SalesOrderLineDelete_Ref](
 		SalesOrder varchar(20),
@@ -15,7 +15,7 @@ begin
 end
 go
 
-if exists(select 1 from [PRODUCT_INFO].[sys].[tables] where [tables].[name] ='SalesOrderLineDeleteExport_Archive')
+if not exists(select 1 from [PRODUCT_INFO].[sys].[tables] where [tables].[name] ='SalesOrderLineDeleteExport_Archive')
 begin
 	create table [SugarCrm].[SalesOrderLineDeleteExport_Archive](
 		SalesOrder varchar(20),
@@ -30,7 +30,7 @@ end;
 go
 
 declare @ExportType as varchar(200) = 'Order Line Items Delete',
-	    @Active as bit = 0
+	    @Active as bit = 1
 
 if exists( select * from [SugarCrm].JobOptions where ExportType = @ExportType)
 begin
@@ -138,6 +138,91 @@ end;
 go
 
 /*
+=========================================================================
+	Created by:		Justin Pope
+	Create Date:	05/26/2023
+	Description:	Flagging proc for the Order Line Items Delete job
+=========================================================================
+	test:
+		execute [PRODUCT_INFO].[SugarCrm].[FlagOrderLineItemsDeleteAsSubmitted]
+=========================================================================
+*/
+create or alter procedure [SugarCrm].[FlagOrderLineItemsDeleteAsSubmitted]
+as
+begin
+
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	SET XACT_ABORT ON;
+	SET DEADLOCK_PRIORITY LOW;
+	
+	BEGIN TRY
+	
+		Begin Transaction;
+
+			delete slr
+			from [PRODUCT_INFO].[SugarCrm].[SalesOrderLine_Ref] as slr
+				inner join [PRODUCT_INFO].[SugarCrm].[SalesOrderLineDelete_Ref] as sldr on sldr.SalesOrder = slr.SalesOrder collate Latin1_General_BIN
+																						and sldr.SalesOrderInitLine = slr.SalesOrderInitLine
+
+			--Archive Export sent
+			insert into [SugarCrm].[SalesOrderLineDeleteExport_Archive] (	[SalesOrder],
+																			[SalesOrderInitLine],
+																			[TimeStamp]
+																			)
+				select
+					[Export].[SalesOrder],
+					[Export].[SalesOrderInitLine],
+					getdate()
+				from [SugarCrm].[tvf_BuildSalesOrderLineDeleteDataset]() as [Export]
+
+			--Delete Submitted Records
+			delete from [PRODUCT_INFO].[SugarCrm].[SalesOrderLineDelete_Ref]
+
+		Commit Transaction;
+	
+
+		BEGIN TRANSACTION;
+
+			-- Purge old audit records
+			DELETE FROM PRODUCT_INFO.SugarCrm.SalesOrderLineExport_Audit
+			WHERE DATEDIFF(day, [TimeStamp], SYSDATETIME()) > (	SELECT  
+																	[AuditRetentionDays]
+																FROM [Global].[Settings].[SugarCrm_Export]
+																WHERE [SiteName] = 'SugarCRM'
+																	AND [DatasetType] = 'SalesOrder_Line' );
+		COMMIT TRANSACTION;
+
+	END TRY
+
+	BEGIN CATCH
+
+		IF @@ROWCOUNT > 0
+			ROLLBACK TRANSACTION;
+
+		SELECT	ERROR_NUMBER()			AS [ErrorNumber]
+						,ERROR_SEVERITY()		AS [ErrorSeverity]
+						,ERROR_STATE()			AS [ErrorState]
+						,ERROR_PROCEDURE()	AS [ErrorProcedure]
+						,ERROR_LINE()				AS [ErrorLine]
+						,ERROR_MESSAGE()		AS [ErrorMessage];
+
+		THROW;
+
+		RETURN 1;
+
+	END CATCH;
+
+	IF @@TRANCOUNT > 0
+	BEGIN
+			ROLLBACK TRANSACTION;
+			RAISERROR('UNEXPECTED ROLLBACK OCCCURRED!' , 20, 1);
+	END
+
+end;
+go
+
+/*
  =============================================
  Author:		Justin Pope
  Create date:	8/11/2022
@@ -213,21 +298,21 @@ as begin
 							[Counts].[ExportType],
 							[Counts].[CNT]
 						from (
-							select 'Accounts' as [ExportType], count(c.Customer) as [CNT] from [SugarCrm].[tvf_BuildCustomerDataset]() as C
+							select @Export_Accounts as [ExportType], count(c.Customer) as [CNT] from [SugarCrm].[tvf_BuildCustomerDataset]() as C
 							union
-							select 'Quotes', count(q.OrderNumber) from [SugarCrm].[tvf_BuildQuoteHeaderDataset]() as Q
+							select @Export_Quotes as [ExportType], count(q.OrderNumber) from [SugarCrm].[tvf_BuildQuoteHeaderDataset]() as Q
 							union
-							select 'Quote Line Items', count(QD.OrderNumber) from [SugarCrm].[tvf_BuildQuoteDetailDataset]() as QD
+							select @Export_QuoteLine as [ExportType], count(QD.OrderNumber) from [SugarCrm].[tvf_BuildQuoteDetailDataset]() as QD
 							union
-							select 'Orders', count(O.SalesOrder) from [SugarCrm].[tvf_BuildSalesOrderHeaderDataset]() as O
+							select @Export_Order as [ExportType], count(O.SalesOrder) from [SugarCrm].[tvf_BuildSalesOrderHeaderDataset]() as O
 							union
-							select 'Order Line Items', count(OD.SalesOrder) from [SugarCrm].[tvf_BuildSalesOrderLineDataset]() as OD
+							select @Export_OrderLine as [ExportType], count(OD.SalesOrder) from [SugarCrm].[tvf_BuildSalesOrderLineDataset]() as OD
 							union
-							select 'Invoices', count(I.Invoice) from [SugarCrm].[tvf_BuildInvoiceDataset]() as I
+							select @Export_Invoices as [ExportType], count(I.Invoice) from [SugarCrm].[tvf_BuildInvoiceDataset]() as I
 							union
-							select 'Invoice Line Items', count(IL.DetailLine) from [SugarCrm].[tvf_BuildInvoiceLineDataset]() as IL
+							select @Export_InvoiceLine as [ExportType], count(IL.DetailLine) from [SugarCrm].[tvf_BuildInvoiceLineDataset]() as IL
 							union
-							select '', count(OLD.[SalesOrderInitLine]) from [SugarCrm].[tvf_BuildSalesOrderLineDeleteDataset]() as OLD ) as [Counts]
+							select @Export_OrderLineDeletes as [ExportType], count(OLD.[SalesOrderInitLine]) from [SugarCrm].[tvf_BuildSalesOrderLineDeleteDataset]() as OLD ) as [Counts]
 						where [Counts].[CNT] > 0
 					) as [Queue] on [Queue].ExportType = ops.ExportType
 	where [Active_Flag] = 1
@@ -290,6 +375,52 @@ as begin
 	from [SugarCRM].[JobOptions] as ops
 	where ops.ExportType = @ExportType
 	for json path, WITHOUT_ARRAY_WRAPPER )
+
+end;
+go
+
+/*
+ =============================================
+ Author:		Justin Pope
+ Create date:	8/15/2022
+ Description:	Flag records as submitted by 
+				Type
+ =============================================
+ Modifier:		Justin Pope
+ Modified Date: 9/16/2022
+ Description:	Adding Invoices and Invoice 
+				Line Items import
+ =============================================
+ Modifier:		Justin Pope
+ Modified Date: 5/26/2023
+ Description:	Adding Order Line Items 
+				Delete import
+ =============================================
+ TEST:
+ execute [SugarCRM].[FlagRecordsAsSubmitted] 'Invoice Line Items'
+ =============================================
+*/
+Create or ALTER procedure [SugarCrm].[FlagRecordsAsSubmitted](
+	@ExportType varchar(50))
+as begin
+	declare @ExportType_Accounts as varchar(50) = 'Accounts',
+			@ExportType_Quotes as varchar(50) = 'Quotes',
+			@ExportType_QuoteLineItems as varchar(50) = 'Quote Line Items',
+			@ExportType_Orders as varchar(50) = 'Orders',
+			@ExportType_OrderLineItems as varchar(50) = 'Order Line Items',
+			@ExportType_Invoices as varchar(50) = 'Invoices',
+			@ExportType_InvoicesLineItems as varchar(50) = 'Invoice Line Items',
+			@ExportType_OrderLineDeletes as varchar(25) = 'Order Line Items Delete'
+
+
+	if @ExportType = @ExportType_Accounts begin execute [SugarCrm].[FlagCustomersAsSubmitted] end
+	if @ExportType = @ExportType_Quotes begin execute [SugarCrm].[FlagQuoteHeadersAsSubmitted] end
+	if @ExportType = @ExportType_QuoteLineItems begin execute [SugarCrm].[FlagQuoteDetailsAsSubmitted] end
+	if @ExportType = @ExportType_Orders begin execute [SugarCrm].[FlagSalesOrderHeadersAsSubmitted] end
+	if @ExportType = @ExportType_OrderLineItems begin execute [SugarCrm].[FlagSalesOrderLinesAsSubmitted] end
+	if @ExportType = @ExportType_Invoices begin execute [SugarCrm].[FlagInvoicesAsSubmitted] end
+	if @ExportType = @ExportType_InvoicesLineItems begin execute [SugarCrm].[FlagInvoiceLinesAsSubmitted] end
+	if @ExportType = @ExportType_OrderLineDeletes begin execute [SugarCrm].[FlagOrderLineItemsDeleteAsSubmitted] end
 
 end;
 go
--- Tickets/Normal Tickets/Ticket 37223 - Job for Deletions/Development/procedure [SugarCrm].[FlagOrderLineItemsDeleteAsSubmitted].sql
+++ Tickets/Normal Tickets/Ticket 37223 - Job for Deletions/Development/procedure [SugarCrm].[FlagOrderLineItemsDeleteAsSubmitted].sql
@@ -1,9 +1,85 @@
 use [PRODUCT_INFO]
 go
 
-/**/
-create or alter procedure [SugarCrm].[FlagInvoiceLinesAsSubmitted]
+/*
+=========================================================================
+	Created by:		Justin Pope
+	Create Date:	05/26/2023
+	Description:	Flagging proc for the Order Line Items Delete job
+=========================================================================
+	test:
+		execute [PRODUCT_INFO].[SugarCrm].[FlagOrderLineItemsDeleteAsSubmitted]
+=========================================================================
+*/
+create or alter procedure [SugarCrm].[FlagOrderLineItemsDeleteAsSubmitted]
 as
 begin
+
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+	SET XACT_ABORT ON;
+	SET DEADLOCK_PRIORITY LOW;
+	
+	BEGIN TRY
+	
+		Begin Transaction;
+
+			delete slr
+			from [PRODUCT_INFO].[SugarCrm].[SalesOrderLineDelete_Ref] as slr
+				inner join [PRODUCT_INFO].[SugarCrm].[SalesOrderLineDelete_Ref] as sldr on sldr.SalesOrder = slr.SalesOrder
+																						and sldr.SalesOrderInitLine = slr.SalesOrderInitLine
+
+			--Archive Export sent
+			insert into [SugarCrm].[SalesOrderLineDeleteExport_Archive] (	[SalesOrder],
+																			[SalesOrderInitLine],
+																			[TimeStamp]
+																			)
+				select
+					[Export].[SalesOrder],
+					[Export].[SalesOrderInitLine],
+					getdate()
+				from [SugarCrm].[tvf_BuildSalesOrderLineDeleteDataset]() as [Export]
+
+			--Delete Submitted Records
+			delete from [PRODUCT_INFO].[SugarCrm].[SalesOrderLineDelete_Ref]
+
+		Commit Transaction;
+
+		BEGIN TRANSACTION;
+
+			-- Purge old audit records
+			DELETE FROM PRODUCT_INFO.SugarCrm.SalesOrderLineExport_Audit
+			WHERE DATEDIFF(day, [TimeStamp], SYSDATETIME()) > (	SELECT  
+																	[AuditRetentionDays]
+																FROM [Global].[Settings].[SugarCrm_Export]
+																WHERE [SiteName] = 'SugarCRM'
+																	AND [DatasetType] = 'SalesOrder_Line' );
+		COMMIT TRANSACTION;
+
+	END TRY
+
+	BEGIN CATCH
+
+		IF @@ROWCOUNT > 0
+			ROLLBACK TRANSACTION;
+
+		SELECT	ERROR_NUMBER()			AS [ErrorNumber]
+						,ERROR_SEVERITY()		AS [ErrorSeverity]
+						,ERROR_STATE()			AS [ErrorState]
+						,ERROR_PROCEDURE()	AS [ErrorProcedure]
+						,ERROR_LINE()				AS [ErrorLine]
+						,ERROR_MESSAGE()		AS [ErrorMessage];
+
+		THROW;
+
+		RETURN 1;
+
+	END CATCH;
+
+	IF @@TRANCOUNT > 0
+	BEGIN
+			ROLLBACK TRANSACTION;
+			RAISERROR('UNEXPECTED ROLLBACK OCCCURRED!' , 20, 1);
+	END
 
 end
