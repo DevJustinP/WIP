6d218e85a0c599004b5ca1423ca71c580b638e5a
Justin Pope <pope2014@gmail.com>
2023-07-10 11:15:49 -0500

    Development for SDM 35527

--- Tickets/Normal Tickets/Ticket 34984 - Invoice Automation/QueryToSelectDispatchNoteToInvoice.sql
+++ Tickets/Normal Tickets/Ticket 34984 - Invoice Automation/QueryToSelectDispatchNoteToInvoice.sql
@@ -1,5 +1,3 @@
-SELECT TOP 000021
-    a.DispatchNote, a.SalesOrder, a.Customer, a.DispatchNoteStatus, a.CustomerPoNumber, a.Invoice, a.PlannedDeliverDate, a.ActualDeliveryDate, a.Branch, a.Salesperson
-FROM [SysproCompany100].[dbo].[MdnMaster] a WITH (NOLOCK)
-WHERE  a.DispatchNoteStatus IN( '0' , '
-3' , '5' , '7' , '8' , 'H' , 'S' ) AND a.DispatchNote >= '' ORDER BY a.DispatchNote
+select
+	*
+from [dbo].FabricSpecification
