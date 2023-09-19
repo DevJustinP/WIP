a07e27c32073e45ad05be72c0a8e1aeb54b56276
Justin Pope <pope2014@gmail.com>
2023-09-19 09:33:41 -0500

    ticket 41958

--- Tickets/Normal Tickets/Ticket 35667 - OptionGroupToProduct new column/Deployment/Deployment.sql
+++ Tickets/Normal Tickets/Ticket 35667 - OptionGroupToProduct new column/Deployment/Deployment.sql
@@ -6,80 +6,116 @@ GO
 */
 alter table [Archive].[OptionGroupToProduct]
 add [Upcharge_R_Old] [smallint] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [Upcharge_R1_Old] [smallint] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [Upcharge_RA_Old] [smallint] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [UploadToEcatRetail_Old] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [UploadToEcatGabbyWholesale_Old] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [UploadToEcatScWholesale_Old] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [UploadToEcatContract_Old] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [DisplayInSkuBuilder_Old] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [ExcludeFromEcatMatrix_Old] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [Upcharge_R_New] [smallint] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [Upcharge_R1_New] [smallint] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [Upcharge_RA_New] [smallint] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [UploadToEcatRetail_New] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [UploadToEcatGabbyWholesale_New] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [UploadToEcatScWholesale_New] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [UploadToEcatContract_New] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [DisplayInSkuBuilder_New] [bit] null;
+go
 alter table [Archive].[OptionGroupToProduct]
 add [ExcludeFromEcatMatrix_New] [bit] null;
+go
 
 /*
 	table [PRODUCT_INFO_Audit].[Stage].[OptionGroupToProduct]
 */
 alter table [Stage].[OptionGroupToProduct]
 add [Upcharge_R_Old] [smallint] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [Upcharge_R1_Old] [smallint] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [Upcharge_RA_Old] [smallint] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [UploadToEcatRetail_Old] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [UploadToEcatGabbyWholesale_Old] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [UploadToEcatScWholesale_Old] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [UploadToEcatContract_Old] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [DisplayInSkuBuilder_Old] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [ExcludeFromEcatMatrix_Old] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [Upcharge_R_New] [smallint] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [Upcharge_R1_New] [smallint] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [Upcharge_RA_New] [smallint] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [UploadToEcatRetail_New] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [UploadToEcatGabbyWholesale_New] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [UploadToEcatScWholesale_New] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [UploadToEcatContract_New] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [DisplayInSkuBuilder_New] [bit] null;
+go
 alter table [Stage].[OptionGroupToProduct]
 add [ExcludeFromEcatMatrix_New] [bit] null;
+go
 
 use [PRODUCT_INFO]
 go
